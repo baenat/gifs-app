49260c7c0eb018979aa1b5e9ed921dae785ab08d
Baenat <bae.julian10@gmail.com>
2025-09-09 11:22:43 -0500

    Configura rutas para el dashboard con rutas hijas para trending y search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard/dashboard.component').then(c => c.DashboardComponent),
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/trending/trending.component').then(c => c.TrendingComponent),
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search/search.component').then(c => c.SearchComponent),
+      },
+      {
+        path: '**',
+        redirectTo: 'trending'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
