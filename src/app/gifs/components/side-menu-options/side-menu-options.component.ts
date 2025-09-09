a2d88366eca73f776b0f7bd98bb906b07967a002
Baenat <bae.julian10@gmail.com>
2025-09-09 14:47:49 -0500

    Añade componente para el menu lateral e implementa sistema de diseño basado en Atomic Design

---
+++ src/app/gifs/components/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+interface MenuOptions {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [],
+  templateUrl: './side-menu-options.component.html',
+  styleUrl: './side-menu-options.component.css'
+})
+export class SideMenuOptionsComponent {
+
+  menuOptions: MenuOptions[] = [
+    {
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+      icon: 'fa-solid fa-chart-line'
+    },
+    {
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search',
+      icon: 'fa-solid fa-magnifying-glass'
+    }
+  ];
+
+}
