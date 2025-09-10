80125c9f4c8351223f4e551c3fdfcb947b606e6b
Baenat <bae.julian10@gmail.com>
2025-09-10 15:47:55 -0500

    Configura GiphyApikey en los ambientes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
   companyName: 'Gifs',
-  companySlogan: 'Gestiona tus Gifs'
+  companySlogan: 'Gestiona tus Gifs',
+
+  giphyApiKey: '2XtGZ4RToMrXR59tarNqmByUm1835wB9',
+  giphyApiUrl: 'https://api.giphy.com/v1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: false,
   companyName: 'Gifs',
-  companySlogan: 'Gestiona tus Gifs'
+  companySlogan: 'Gestiona tus Gifs',
+
+  giphyApiKey: '2XtGZ4RToMrXR59tarNqmByUm1835wB9',
+  giphyApiUrl: 'https://api.giphy.com/v1',
 };
