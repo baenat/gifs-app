f0ac53b721c94988a7aea0f9b14dcdd6b7514ade
Baenat <bae.julian10@gmail.com>
2025-09-10 15:49:18 -0500

    Implementa interfaces y mapper de la data gifs a gestionar

---
+++ src/app/gifs/mapper/gif.mapper.ts
@@ -0,0 +1,18 @@
+import { Gif } from "../interfaces/gif.interface";
+import { GiphyItem } from "../interfaces/giphy.interface";
+
+export class GifMapper {
+
+  static mapGiphyToGif(item: GiphyItem): Gif {
+    return {
+      id: item.id,
+      title: item.title,
+      url: item.images.original.url
+    }
+  }
+
+  static mapGiphyToGifArray(items: GiphyItem[]): Gif[] {
+    return items.map(this.mapGiphyToGif);
+  }
+
+}
